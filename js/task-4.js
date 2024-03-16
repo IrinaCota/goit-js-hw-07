af13cc5527a2dd775a34eeba76be0fba1aa687f0
IrinaCota <iryna.kotsa91@gmail.com>
2024-03-16 17:22:11 +0000

    task 4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const form = document.querySelector(".login-form");
 
+form.addEventListener("submit", event => {
+    event.preventDefault();
+    
+    const email = event.currentTarget.elements.email.value;
+    const password = event.currentTarget.elements.password.value;
+
+    const formValues = {
+        email,
+        password,
+    };
+    console.log(formValues);
+
+    if (email === "" || password === "") {
+        return console.log("All form fields must be filled in");
+    }
+
+    event.currentTarget.reset();
+});
